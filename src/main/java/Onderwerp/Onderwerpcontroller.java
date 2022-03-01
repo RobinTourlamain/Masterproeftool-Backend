7899076cafb639d807f04e3dee3329db5350275c
RobinTourlamain <robin.tourlamain@gmail.com>
2022-03-01 10:51:16 +0100

    Controller testresponse

--- src/main/java/Onderwerp/Onderwerpcontroller.java
+++ src/main/java/Onderwerp/Onderwerpcontroller.java
@@ -1,8 +1,11 @@
 package Onderwerp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -12,5 +15,10 @@ public class Onderwerpcontroller {
     public Onderwerpcontroller(Onderwerpservice onderwerpservice){
     this.onderwerpservice = onderwerpservice;
     }
-    
+
+    @GetMapping(path = "/")  //testmapping
+    //@ResponseBody                   //String gaat rechtstreeks naar http body
+    public ResponseEntity<String> sayHello(){
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
 }
