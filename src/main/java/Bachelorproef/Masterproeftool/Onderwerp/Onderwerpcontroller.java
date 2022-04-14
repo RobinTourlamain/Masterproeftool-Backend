de8f26440f74ad8b47a639686cb3b939c3a65cee
fiendeleersnyder <fien.deleersnyder@student.kuleuven.be>
2022-04-14 17:07:27 +0200

    favorieten (array)

--- src/main/java/Bachelorproef/Masterproeftool/Onderwerp/Onderwerpcontroller.java
+++ src/main/java/Bachelorproef/Masterproeftool/Onderwerp/Onderwerpcontroller.java
@@ -1,5 +1,6 @@
 package Bachelorproef.Masterproeftool.Onderwerp;
 
+import Bachelorproef.Masterproeftool.Authenticatie.Gebruiker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +17,7 @@ import java.util.Optional;
 @RestController //controller en responsebody ineen
 public class Onderwerpcontroller {
     private final Onderwerpservice onderwerpservice;
+    private ArrayList<Integer> favorieten = new ArrayList<>();
     @Autowired
     public Onderwerpcontroller(Onderwerpservice onderwerpservice){
     this.onderwerpservice = onderwerpservice;
@@ -34,6 +37,7 @@ public class Onderwerpcontroller {
     Onderwerp getOnderwerpById(@PathVariable int id){
         return onderwerpservice.getOnderwerpById(id);
     }
+
     @PutMapping(path = "/onderwerpen/{id}")
     Onderwerp vervangOnderwerp(@RequestBody Onderwerp temponderwerp, @PathVariable int id){
         return onderwerpservice.vervangOnderwerp(temponderwerp, id);
@@ -45,5 +49,22 @@ public class Onderwerpcontroller {
     @DeleteMapping(path = "/onderwerpen/{id}")
     void deleteOnderwerp(@PathVariable int id) {
         onderwerpservice.deleteOnderwerp(id);
+    }
+
+    @GetMapping(path = "/favorieten")
+    ArrayList<Integer> favorieten(){ //username zal worden meegegeven in body dus moet die hier eruit gehaald worden
+        return favorieten; //momenteel heb ik gwn een lijst aangemaakt voor te testen of mn frontend werkt, uiteindelijk moet dit uit de database gehaald worden
+    }
+
+    @PostMapping(path = "/addfavoriet/{id}")
+    Onderwerp favorietOnderwerp(@PathVariable int id) {
+        favorieten.add(id);
+        return onderwerpservice.getOnderwerpById(id);
+    }
+
+    @DeleteMapping(path = "/deletefavoriet/{id}")
+    Onderwerp deleteFavorietOnderwerp(@PathVariable int id) {
+        favorieten.remove(favorieten.indexOf(id));
+        return onderwerpservice.getOnderwerpById(id);
     }
 }
