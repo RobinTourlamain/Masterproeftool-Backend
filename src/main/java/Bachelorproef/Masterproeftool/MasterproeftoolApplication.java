f0135184411cc29e8f28cdd10cbebfd559ba8ee6
fiendeleersnyder <fien.deleersnyder@student.kuleuven.be>
2022-04-24 12:12:35 +0200

    allowedMethods toegevoegd voor CORS

--- src/main/java/Bachelorproef/Masterproeftool/MasterproeftoolApplication.java
+++ src/main/java/Bachelorproef/Masterproeftool/MasterproeftoolApplication.java
@@ -23,6 +23,7 @@ public class MasterproeftoolApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "DELETE", "OPTIONS", "PUT", "PATCH" )
 						.allowCredentials(true);
 			}
 		};
