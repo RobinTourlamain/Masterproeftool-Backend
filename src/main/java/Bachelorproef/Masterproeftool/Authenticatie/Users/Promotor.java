a5a271481062c906223d66e54334f69b5d147acb
RobinTourlamain <robin.tourlamain@gmail.com>
2022-05-09 15:28:04 +0200

    promotor constructor

--- src/main/java/Bachelorproef/Masterproeftool/Authenticatie/Users/Promotor.java
+++ src/main/java/Bachelorproef/Masterproeftool/Authenticatie/Users/Promotor.java
@@ -4,8 +4,21 @@ import Bachelorproef.Masterproeftool.Authenticatie.Gebruiker;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import java.util.ArrayList;
 
 @Entity @Data
 public class Promotor extends Gebruiker{
     private String firstname;
+
+
+
+    public Promotor(String n, String u, String p, ArrayList r, String fn){
+        super( n,  u,  p, r);
+        this.firstname = fn;
+    }
+
+    public Promotor() {
+    }
 }
+
+
